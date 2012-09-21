dc480eb974806b4beb7bc8ead8c153c64464f476
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-09-21 14:03:41 -0400

    initial commit
    new project from 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyManager::Application.config.secret_token = 'afc7c6c9534cf1d79ef6056f08f0eac7bc3812d7d3b1ecb328f8f61cc8e6fe9ebf2b9f56f1204325f6641ee70969664c42fc3518ad55746352ab66c784b07dec'

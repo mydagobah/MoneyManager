04d51121b5f2f7e37f5c8bd6305347388cd5ffec
Wenjun Zhang <wenjunzhang.surpass@gmail.com>
2012-10-03 19:43:43 -0400

    tune UI

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ MoneyManager::Application.routes.draw do
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
-
+  match '/about',   to: 'welcome#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
